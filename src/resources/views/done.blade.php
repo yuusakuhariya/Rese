6009052aad1c1e12bc302497d9ba684cb7dc8d91
yuusaku <yuusaku08030213@gmail.com>
2024-02-26 22:12:43 +0900

    done.css 完成

--- src/resources/views/done.blade.php
+++ src/resources/views/done.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('/css/done.css') }}" />
+@endsection
+
 @section('content')
 
 <main>
@@ -8,7 +12,7 @@
             <div class="content_message">
                 ご予約ありがとうございます
             </div>
-            <div><a href="">戻る</a></div>
+            <div class="content_message_back"><a href="">戻る</a></div>
         </div>
     </div>
 </main>
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <title>Rese</title>
     <link rel="stylesheet" href="{{ asset('/css/sanitize.css') }}" />
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}" />
+    @yield('css')
 </head>
 
 <body>
